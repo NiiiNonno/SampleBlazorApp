f7a5b8312fe04fc40a197a0bdcc7464c1a97d663
niii.nonno@gmail.com <niii.nonno@gmail.com>
2022-11-13 20:06:02 +0900

    サブモジュールの削除。

--- Assets/Assets.Graphics/IBroadcaster.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nonno.Assets.Graphics;
-
-public interface IBroadcaster
-{
-    decimal FrameRate { get; set; }
-    double Quality { get; set; }
-
-    Task GetNextFrame(Stream to, string type);
-}
--- Assets/Assets.Graphics/_Usings.cs
+++
@@ -1,5 +0,0 @@
-﻿#if USE_DOUBLE
-global using Dec = System.Double;
-#else
-global using Dec = System.Single;
-#endif
