55b2c898ca1ac2b19608200775c7b342a48899df
niii.nonno@gmail.com <niii.nonno@gmail.com>
2022-11-17 11:05:43 +0900

    網繹典版にプレゼンテーション管理機能の追加試行。

---
+++ SampleBlazorApp.WebAssembly/Shared/PlesentationTimer.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Timer = System.Timers.Timer;
+
+namespace Nonno.SampleBlazorApp.WebAssembly.Shared;
+
+public partial class PlesentationTimer
+{
+    readonly Timer _timer;
+    TimeSpan _rest;
+    TimeSpan _limit;
+    string _buttonLabel;
+
+    [Parameter]
+    public TimeSpan Limit
+    {
+        get => _limit;
+        set
+        {
+            _limit = value;
+            _rest = value;
+        }
+    }
+
+    public PlesentationTimer()
+    {
+        _buttonLabel = "開始";
+        _timer = new Timer();
+        _timer.Interval = 1000;
+        _timer.Elapsed += (_, _) =>
+        {
+            _rest -= new TimeSpan(0, 0, 1);
+
+            StateHasChanged();
+        };
+    }
+
+    public void ButtonClick()
+    {
+        _timer.Enabled = !_timer.Enabled;
+        _buttonLabel = _timer.Enabled ? "停止" : "開始";
+    }
+}
